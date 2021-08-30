fd106c0a73849b6b6a4dfa068f30bda5bb7b676d
PL-O <80671477+PL-O@users.noreply.github.com>
2021-08-31 06:06:47 +0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from './page/Navbar'
+import Header from './page/Header'
+import Homepage from './page/Homepage'
+import Footer from './page/Footer'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar/>
+      <Header/>
+      <Homepage/>
+      <Footer/>
+    </>
   );
 }
 
